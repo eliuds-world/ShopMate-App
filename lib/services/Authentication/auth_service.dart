4b3c534652a272ca56ea1e38f026ec3497fbb99f
eliuds_world <mbuthiaeliud895@gmail.com>
2023-10-08 09:58:01 +0300

    commenting some files for later use

--- lib/services/Authentication/auth_service.dart
+++ lib/services/Authentication/auth_service.dart
@@ -1,43 +1,43 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
-import 'package:shopmate/services/Authentication/auth_provider.dart';
-import 'package:shopmate/services/Authentication/auth_user.dart';
-import 'package:shopmate/services/Authentication/firebase_auth_providers.dart';
+// import 'package:shopmate/services/Authentication/auth_provider.dart';
+// import 'package:shopmate/services/Authentication/auth_user.dart';
+// import 'package:shopmate/services/Authentication/firebase_auth_providers.dart';
 
-class AuthService implements AuthProvider {
-  final AuthProvider provider;
-  const AuthService(this.provider);
+// class AuthService implements AuthProvider {
+//   final AuthProvider provider;
+//   const AuthService(this.provider);
 
-  factory AuthService.firebase() => AuthService(
-      FirebaseAuthProvider(),
-      );
+//   factory AuthService.firebase() => AuthService(
+//       FirebaseAuthProvider(),
+//       );
 
-  @override
-  Future<AuthUser> createUser({
-    required String email,
-    required String password,
-  }) =>
-      provider.createUser(
-        email: email,
-        password: password,
-      );
+//   @override
+//   Future<AuthUser> createUser({
+//     required String email,
+//     required String password,
+//   }) =>
+//       provider.createUser(
+//         email: email,
+//         password: password,
+//       );
 
-  @override
-  AuthUser? get currentUser => provider.currentUser;
+//   @override
+//   AuthUser? get currentUser => provider.currentUser;
 
-  @override
-  Future<AuthUser> logIn({
-    required String email,
-    required String password,
-  }) =>
-      provider.logIn(
-        email: email,
-        password: password,
-      );
+//   @override
+//   Future<AuthUser> logIn({
+//     required String email,
+//     required String password,
+//   }) =>
+//       provider.logIn(
+//         email: email,
+//         password: password,
+//       );
 
-  @override
-  Future<Void> logOut() => provider.logOut();
+//   @override
+//   Future<Void> logOut() => provider.logOut();
 
-  @override
-  Future<void> initialize() => provider.initialize();
-}
+//   @override
+//   Future<void> initialize() => provider.initialize();
+// }
--- lib/services/Authentication/firebase_auth_providers.dart
+++ lib/services/Authentication/firebase_auth_providers.dart
@@ -98,77 +98,77 @@
 // }
 // }
 
-import 'dart:ffi';
+// import 'dart:ffi';
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:shopmate/firebase_options.dart';
-import 'package:shopmate/services/Authentication/auth_provider.dart';
-import 'package:shopmate/services/Authentication/auth_user.dart';
-import 'package:shopmate/services/Authentication/exceptions.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:shopmate/firebase_options.dart';
+// import 'package:shopmate/services/Authentication/auth_provider.dart';
+// import 'package:shopmate/services/Authentication/auth_user.dart';
+// import 'package:shopmate/services/Authentication/exceptions.dart';
 
-class FirebaseAuthProvider implements AuthProvider {
-  @override
-  Future<AuthUser> createUser({
-    required String email,
-    required String password,
-  }) async {
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    final user = currentUser;
-    if (user != null) {
-      return user;
-    } else {
-      throw UserNotLoggedInAuthException();
-    }
-  }
+// class FirebaseAuthProvider implements AuthProvider {
+//   @override
+//   Future<AuthUser> createUser({
+//     required String email,
+//     required String password,
+//   }) async {
+//     await FirebaseAuth.instance.createUserWithEmailAndPassword(
+//       email: email,
+//       password: password,
+//     );
+//     final user = currentUser;
+//     if (user != null) {
+//       return user;
+//     } else {
+//       throw UserNotLoggedInAuthException();
+//     }
+//   }
 
-  @override
-  AuthUser? get currentUser {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      return AuthUser.fromFirebase(user);
-    } else {
-      return null;
-    }
-  }
+//   @override
+//   AuthUser? get currentUser {
+//     final user = FirebaseAuth.instance.currentUser;
+//     if (user != null) {
+//       return AuthUser.fromFirebase(user);
+//     } else {
+//       return null;
+//     }
+//   }
 
-  @override
-  Future<AuthUser> logIn({
-    required String email,
-    required String password,
-  }) async {
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      final user = currentUser;
-      if (user != null) {
-        return user;
-      } else {
-        throw Exception();
-      }
-    } on FirebaseAuthException catch (error) {}
+//   @override
+//   Future<AuthUser> logIn({
+//     required String email,
+//     required String password,
+//   }) async {
+//     try {
+//       await FirebaseAuth.instance.signInWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
+//       final user = currentUser;
+//       if (user != null) {
+//         return user;
+//       } else {
+//         throw Exception();
+//       }
+//     } on FirebaseAuthException catch (error) {}
 
-    @override
-    Future<Void> logOut() async {
-      final user = FirebaseAuth.instance.currentUser;
-      if (user != null) {
-        await FirebaseAuth.instance.signOut();
-      } else {
-        throw Exception();
-      }
-      throw Exception();
-    }
+//     @override
+//     Future<Void> logOut() async {
+//       final user = FirebaseAuth.instance.currentUser;
+//       if (user != null) {
+//         await FirebaseAuth.instance.signOut();
+//       } else {
+//         throw Exception();
+//       }
+//       throw Exception();
+//     }
 
-    @override
-    Future<void> initialize() async {
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-    }
-  }
-}
+//     @override
+//     Future<void> initialize() async {
+//       await Firebase.initializeApp(
+//         options: DefaultFirebaseOptions.currentPlatform,
+//       );
+//     }
+//   }
+// }
